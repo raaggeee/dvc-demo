e30e22d229083048fd10058bcce6262e9bc65620
raaggeee <raaggee08@gmail.com>
2026-03-05 16:08:41 +0530

    data exp

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,7 +6,7 @@ df = pd.read_csv("/data2/dvc-demo/data/smile-annotations-final.csv")
 df.columns = ["id", "tweets", "label"]
 
 df = df.iloc[:, 1:]
-df = df[df["tweets"].str.len() < 500]
+df = df[df["tweets"].str.len() < 100]
 os.makedirs("/data2/dvc-demo/data/raw", exist_ok=True)
 os.chdir("/data2/dvc-demo/data/raw")
 df.to_csv("new_data.csv")
