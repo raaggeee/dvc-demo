9f6f429ce8521d2d9cb738d5f2fc955ae41ea6cc
raaggeee <raaggee08@gmail.com>
2026-03-05 16:02:46 +0530

    add first experiemnt

---
+++ src/data_ingestion.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os
+
+df = pd.read_csv("/data2/dvc-demo/data/smile-annotations-final.csv")
+df.columns = ["id", "tweets", "label"]
+
+df = df.iloc[:, 1:]
+df = df[df["tweets"].str.len() < 500]
+os.makedirs("/data2/dvc-demo/data/raw", exist_ok=True)
+os.chdir("/data2/dvc-demo/data/raw")
+df.to_csv("new_data.csv")
